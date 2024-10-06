463924f1985976d718e6385cdbad84f215b0ca30
SobaSkee <117794857+SobaSkee@users.noreply.github.com>
2024-10-06 15:33:11 -0400

    refactor(page): reorganize components into new folder with path aliasing

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import Navbar from "@components/Navbar";
+import Hero from "@components/Hero";
+
 export default function Home() {
-  return <div className="container mx-auto ">Gator AI</div>;
+  return (
+    <div className="container mx-auto ">
+      <Navbar></Navbar>
+      <Hero></Hero>
+    </div>
+  );
 }
