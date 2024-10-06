dbdd08a030fece7ee7f3441d0c6829749514014e
SobaSkee <117794857+SobaSkee@users.noreply.github.com>
2024-10-06 16:03:34 -0400

    feat: installed shadcn ui library

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,11 @@ import Hero from "@components/Hero";
 export default function Home() {
   return (
     <div className="container mx-auto ">
-      <Navbar></Navbar>
-      <Hero></Hero>
+      {/* wrapper */}
+      <div className="w-full" style={{ backgroundColor: "#0C262A" }}>
+        <Navbar></Navbar>
+        <Hero></Hero>
+      </div>
     </div>
   );
 }
