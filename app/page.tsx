08f1d7a91d4b96c63104bb40e5b9eac1d7907dff
patrickklapid@gmail.com <patrickklapid@gmail.com>
2024-10-06 15:54:55 -0400

    feat: hero header

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import Hero from "@components/Hero";
 
 export default function Home() {
   return (
-    <div className="container mx-auto ">
-      <Navbar></Navbar>
-      <Hero></Hero>
-    </div>
+    <>
+      <Navbar />
+      <div className="container mx-auto">
+        <Hero />
+      </div>
+    </>
   );
 }
