4e1dfbe8e461fb636afd039d6eb0efc035cd92a4
Stanley <kestanley101@gmail.com>
2024-12-14 22:14:34 -0500

    refactor: layout and components

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Image from "next/image";
+import StarIcon from "../images/icons/star-icon.png";
 
 function Navbar() {
-  return <div className="w-full flex items-center bg-red-500">bruh</div>;
+  return (
+    <div className="w-full flex items-center bg-red-500">
+      <div>
+        <Image src={StarIcon} alt="Star Icon" width={50} height={50} />
+        <p>GatorAI</p>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
