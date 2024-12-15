208816e30d30635d451d2136f4e1344e5c8b039c
Stanley <kestanley101@gmail.com>
2024-12-14 21:38:46 -0500

    fix: folder Organization

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Navbar() {
-  return <div>test</div>;
+  return <div className="w-full flex items-center bg-red-500">bruh</div>;
 }
 
 export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Navbar from "@components/Navbar";
 import Hero from "@components/Hero";
 import "./globals.css";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <div className="container mx-auto">
         <Hero />
       </div>
